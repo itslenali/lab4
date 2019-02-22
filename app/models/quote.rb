6e63ec7f5beff1c7549bfbfe2da4de0bb4a0a32c
Lena Li <lenal@andrew.cmu.edu>
2019-02-22 21:35:55 +0000

    added quote require body

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,12 @@
 class Quote < ApplicationRecord
+    
+    # Method to get a random quote for home page #
+# @return [Quote]
+# the random quote
+@return [Quote]
+def self.get_random_quote
+    self.all.sample
+    validates_presence_of :body
+end
+
 end
