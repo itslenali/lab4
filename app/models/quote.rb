52c7db9c08d5fa49a09bd43e97acabe3cfce1744
Lena Li <lenal@andrew.cmu.edu>
2019-02-22 21:58:25 +0000

    finished

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,10 +3,12 @@ class Quote < ApplicationRecord
     # Method to get a random quote for home page #
 # @return [Quote]
 # the random quote
-@return [Quote]
+#@return [Quote]
 def self.get_random_quote
     self.all.sample
-    validates_presence_of :body
+
 end
+
+    validates_presence_of :body
 
 end
